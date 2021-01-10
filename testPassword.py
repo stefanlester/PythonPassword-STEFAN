1c3e1e3d029f26431d57d2298fcba3874ee812cb
stefanlester <38575516+stefanlester@users.noreply.github.com>
2021-01-10 15:34:08 +0100

    provoking vulneranilites

--- testPassword.py
+++ testPassword.py
@@ -1,11 +1,16 @@
 import unittest
 from Password import passwordTester as Password
+from User import User
 
 class TestPassword(unittest.TestCase):
 
     def setUp(self):
         self.password = '123_x&5s'.encode()
 
+    def test_user(self):
+        User.set_name(self, name = "Stefan")
+        response = User.get_name(self)
+        self.assertTrue(response, "Stefan")
 
     def test_hash_password_hash_check(self):
         hashed_pwd = Password.hash_password(self.password)
